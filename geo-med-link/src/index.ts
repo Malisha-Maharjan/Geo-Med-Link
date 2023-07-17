54a8e970de09c3b5ec4c8cdb0e5362644d4d8cb9
Malisha Maharjan <malishamaharjan00@gmail.com>
2023-07-17 13:49:59 +0545

    refactoring

--- geo-med-link/src/index.ts
+++ geo-med-link/src/index.ts
@@ -1,24 +1,11 @@
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
+import { StatusCodes } from "http-status-codes";
 import { ZodError } from "zod";
 import { AppDataSource } from "./data-source";
-import { login } from "./routes/login_routes/login";
-import {
-  activateDonor,
-  deactivateDonor,
-  registerDonor,
-} from "./routes/user_routes/blood-donor";
-import {
-  createUser,
-  deleteUser,
-  getAllUser,
-  getUser,
-  updateUser,
-} from "./routes/user_routes/user";
-// import { loadEnv } from "./utils/env";
-import { StatusCodes } from "http-status-codes";
 import { verifyToken } from "./routes/login_routes/jwt";
-import { env } from "./utils/env";
+
+import { routes } from "./routes";
 import { createResponse } from "./utils/response";
 const app = express();
 
@@ -47,30 +34,16 @@ async function main() {
 
       if (verifyToken(token.split(" ")[1])) {
         console.log({ token });
-        next();
       }
     }
     next();
   });
 
   app.get("/", async (req, res) => {
-    // console.log(createToken());
-    // const token =
-    //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiTWFsaXNoYSIsImlhdCI6MTY4OTMzMjQzN30.1JmLEvxIu_vs4bIj5UZWWRhXoGhu8PzAZuqah9s4tSI";
-    // console.log(verifyToken(token));
-    console.log(JSON.stringify(env, null, 2));
     return res.send("Geo Med Link");
   });
 
-  app.use(getAllUser);
-  app.use(registerDonor);
-  app.use(createUser);
-  app.use(getUser);
-  app.use(deleteUser);
-  app.use(updateUser);
-  app.use(deactivateDonor);
-  app.use(activateDonor);
-  app.use(login);
+  app.use(routes);
 
   app.use((e: Error, req: Request, res: Response, next: NextFunction) => {
     if (!e) {
---
+++ geo-med-link/src/routes.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { login } from "./routes/login_routes/login";
+import {
+  activateDonor,
+  deactivateDonor,
+  registerDonor,
+} from "./routes/user_routes/blood-donor";
+import {
+  createUser,
+  deleteUser,
+  getAllUser,
+  getUser,
+  updateUser,
+} from "./routes/user_routes/user";
+
+const app = express();
+
+export const routes =
+  (app.use(getAllUser),
+  app.use(registerDonor),
+  app.use(createUser),
+  app.use(getUser),
+  app.use(deleteUser),
+  app.use(updateUser),
+  app.use(deactivateDonor),
+  app.use(activateDonor),
+  app.use(login));
