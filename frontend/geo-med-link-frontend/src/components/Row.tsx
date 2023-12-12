44a630d126141558ce1eb6c500ecd50292790a73
Malisha Maharjan <malishamaharjan00@gmail.com>
2023-12-12 22:36:38 +0545

    customized Row component

---
+++ frontend/geo-med-link-frontend/src/components/Row.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+import { View, ViewStyle } from "react-native";
+
+type RowParams = {
+  children?: ReactNode | undefined;
+  style?: ViewStyle;
+};
+
+export const Row = ({ children, style }: RowParams) => {
+  return <View style={[{ flexDirection: "row" }, style]}>{children}</View>;
+};
