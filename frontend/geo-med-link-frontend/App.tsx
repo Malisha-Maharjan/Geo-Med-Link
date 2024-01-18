bdab3b43906a1b390126132dbdbbe0c3203de256
Malisha Maharjan <malishamaharjan00@gmail.com>
2024-01-18 21:24:36 +0545

    theme

--- frontend/geo-med-link-frontend/App.tsx
+++ frontend/geo-med-link-frontend/App.tsx
@@ -3,20 +3,22 @@ import { NavigationContainer } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StyleSheet } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { PaperProvider } from "react-native-paper";
+import { MD3LightTheme, PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { UserContextProvider } from "~/context/userContext";
 import { useUser } from "~/hooks/user/useUser";
 import { RootStackNavigator } from "~/navigations/Root/root-stack";
+import { theme } from "~/theme/theme";
 
 const queryClient = new QueryClient();
 export default function App() {
+  const paperTheme = { ...MD3LightTheme, colors: theme.light };
   const user = useUser();
   return (
     <QueryClientProvider client={queryClient}>
       <UserContextProvider value={user}>
         <GestureHandlerRootView style={{ flex: 1 }}>
-          <PaperProvider>
+          <PaperProvider theme={paperTheme}>
             <BottomSheetModalProvider>
               <SafeAreaProvider>
                 <NavigationContainer>
