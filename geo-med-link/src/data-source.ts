438d11a643cd64fc80dfa04864afe441de000eb2
Malisha Maharjan <malishamaharjan00@gmail.com>
2024-02-25 10:11:06 +0545

    data-source

--- geo-med-link/src/data-source.ts
+++ geo-med-link/src/data-source.ts
@@ -4,6 +4,7 @@ import { User } from "./entity/User";
 // import { Service } from "./entity/services";
 import { Comment } from "./entity/Comment";
 import { Doctor } from "./entity/Doctor";
+import { Event } from "./entity/Event";
 import { Like } from "./entity/Like";
 import { Organization } from "./entity/Organization";
 import { People } from "./entity/People";
@@ -11,28 +12,48 @@ import { Post } from "./entity/Post";
 import { Scrap } from "./entity/scrapNews";
 import { env } from "./utils/env";
 
-export const AppDataSource = new DataSource({
-  type: "mysql",
-  host: env.DATABASE_HOST,
-  port: env.DATABASE_PORT,
-  username: env.DATABASE_USER,
-  password: env.DATABASE_PASSWORD,
-  database: env.DATABASE_NAME,
-  logging: env.DATABASE_LOGGING,
-  entities: [User, People, Organization, Doctor, Scrap, Post, Comment, Like],
-  migrations: [env.MIGRATION_URL],
-  subscribers: [],
-});
-
 // export const AppDataSource = new DataSource({
 //   type: "mysql",
-//   host: "MYSQL5048.site4now.net",
-//   // port: 3306,
-//   username: "aa486b_geomed",
-//   password: "Malisha2001",
-//   database: "db_aa486b_geomed",
-//   // logging: env.DATABASE_LOGGING,
-//   entities: [User, People, Organization, Doctor, Scrap, Post, Comment, Like],
+//   host: env.DATABASE_HOST,
+//   port: env.DATABASE_PORT,
+//   username: env.DATABASE_USER,
+//   password: env.DATABASE_PASSWORD,
+//   database: env.DATABASE_NAME,
+//   logging: env.DATABASE_LOGGING,
+//   entities: [
+//     User,
+//     People,
+//     Organization,
+//     Doctor,
+//     Scrap,
+//     Post,
+//     Comment,
+//     Like,
+//     Event,
+//   ],
 //   migrations: [env.MIGRATION_URL],
 //   subscribers: [],
 // });
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "MYSQL5048.site4now.net",
+  // port: 3306,
+  username: "aa486b_geomed",
+  password: "Malisha2001",
+  database: "db_aa486b_geomed",
+  // logging: env.DATABASE_LOGGING,
+  entities: [
+    User,
+    People,
+    Organization,
+    Doctor,
+    Scrap,
+    Post,
+    Comment,
+    Like,
+    Event,
+  ],
+  migrations: [env.MIGRATION_URL],
+  subscribers: [],
+});
